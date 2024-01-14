d1436336509527017da3f08796185db5bd4381c3
Ahsan Moin <ahsanmoin05@gmail.com>
2024-01-14 12:26:35 -0500

    Fixed useRef declaration problem

--- src/Chemistry.tsx
+++ src/Chemistry.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import Navbar from "./components/Navbar";
 import AnswerChoice from "./components/AnswerChoice";
